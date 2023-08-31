ad27a0759f046e617fedfafc65aed8c202cd98e3
Vinicius Giacomelli <vinicius.giacomelli@fatec.sp.gov.br>
2023-08-31 16:03:12 -0300

    ap01(java): hello world
    - Usando JOptionPane

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
+import javax.swing.JOptionPane;
 public class HelloWorld{
 
     public static void main(String [] args){
         //stdin stdout stderr
-        System.out.println("Hello, World");
+        //System.out.println("Hello, World");
+        JOptionPane.showMessageDialog(null, "Hello, World!");
     }
 }
